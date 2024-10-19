e1aace37651e26477c5ae77d545d380e83394403
AliSabet81 <sabet.farshid1381@gmail.com>
2024-10-19 15:45:06 +0330

    setting up tanstack react query and starting login api

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
+import auth from "@/features/auth/server/route";
+
 const app = new Hono().basePath("/api");
 
 // eslint-disable-next-line
-const routes = app;
+const routes = app.route("/auth", auth);
 
 export const GET = handle(app);
 export const POST = handle(app);
