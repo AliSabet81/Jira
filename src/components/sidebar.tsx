37d081f700322736f394a25469afcc4aedc6f170
AliSabet81 <sabet.farshid1381@gmail.com>
2024-10-22 09:51:25 +0330

    workspaces switcher

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 import { DottedSeparator } from "./dotted-separator";
 import { Navigation } from "./navigation";
+import { WorkspaceSwitcher } from "./workspace-switcher";
 
 export const Sidebar = () => {
   return (
@@ -17,6 +18,8 @@ export const Sidebar = () => {
           priority
         />
       </Link>
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <Navigation />
     </aside>
