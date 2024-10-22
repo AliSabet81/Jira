b98c00b869802dd560343e7f635a7900d3a80228
AliSabet81 <sabet.farshid1381@gmail.com>
2024-10-22 13:21:19 +0330

    setting page

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/actions";
 import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
+import { getWorkspace } from "@/features/workspaces/actions";
 
 interface WorkspaceIdSettingsPageProps {
   params: {
@@ -18,9 +19,15 @@ const WorkspaceIdSettingsPage = async ({
     redirect("/sign-in");
   }
 
+  const initialValues = await getWorkspace({ workspaceId: params.workspaceId });
+
+  if (!initialValues) {
+    redirect(`/workspaces/${params.workspaceId}`);
+  }
+
   return (
     <div className="w-full lg:max-w-xl">
-      <EditWorkspaceForm />
+      <EditWorkspaceForm initialValues={initialValues} />
     </div>
   );
 };
