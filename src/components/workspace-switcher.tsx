c987d48e87e372420790914c4fa8a06732e35abb
AliSabet81 <sabet.farshid1381@gmail.com>
2024-10-22 11:50:23 +0330

    create workspace modal

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -6,6 +6,7 @@ import { RiAddCircleFill } from "react-icons/ri";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+import { useCreateWorkspaceModal } from "@/features/workspaces/hooks/use-create-workspace-modal";
 
 import {
   Select,
@@ -18,6 +19,7 @@ import {
 export const WorkspaceSwitcher = () => {
   const workspaceId = useWorkspaceId();
   const router = useRouter();
+  const { open } = useCreateWorkspaceModal();
   const { data: wokspaces } = useGetWorkspaces();
 
   const onSelect = (id: string) => {
@@ -28,7 +30,10 @@ export const WorkspaceSwitcher = () => {
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
         <p className="text-xs uppercase text-neutral-500">Workspaces</p>
-        <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
+        <RiAddCircleFill
+          onClick={open}
+          className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"
+        />
       </div>
       <Select onValueChange={onSelect} value={workspaceId}>
         <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
