54c778cb7e4594cd8d66dc95e322b8d5e70d9f08
AliSabet81 <sabet.farshid1381@gmail.com>
2024-10-22 10:28:32 +0330

    adding workspace member

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -3,9 +3,15 @@ import { ID } from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
 
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
+import {
+  DATABASE_ID,
+  IMAGES_BUCKET_ID,
+  MEMBERS_ID,
+  WORKSPACES_ID,
+} from "@/config";
 
 import { createWorkspaceSchema } from "../schemas";
+import { MemberRole } from "@/features/members/types";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
@@ -29,7 +35,6 @@ const app = new Hono()
       const { name, image } = c.req.valid("form");
 
       let uploadedImageUrl: string | undefined;
-
       if (image instanceof File) {
         const file = await storage.createFile(
           IMAGES_BUCKET_ID,
@@ -41,7 +46,6 @@ const app = new Hono()
           IMAGES_BUCKET_ID,
           file.$id
         );
-
         uploadedImageUrl = `data:image/png;base64,${Buffer.from(
           arrayBuffer
         ).toString("base64")}`;
@@ -58,8 +62,13 @@ const app = new Hono()
         }
       );
 
+      await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+        userId: user.$id,
+        workspaceId: workspace.$id,
+        role: MemberRole.ADMIN,
+      });
+
       return c.json({ data: workspace });
     }
   );
-
 export default app;
