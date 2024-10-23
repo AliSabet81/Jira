81e4675509f470b6dfcb06eb880619ed21f3cf16
AliSabet81 <sabet.farshid1381@gmail.com>
2024-10-23 15:11:27 +0330

    starting project page

---
+++ src/features/projects/queries.ts
@@ -0,0 +1,35 @@
+import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { getMember } from "../members/utils";
+import { createSessionClient } from "@/lib/appwrite";
+import { Project } from "./types";
+
+interface GetProjectProps {
+  projectId: string;
+}
+
+export const getProject = async ({ projectId }: GetProjectProps) => {
+  try {
+    const { account, databases } = await createSessionClient();
+    const user = await account.get();
+
+    const project = await databases.getDocument<Project>(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      projectId
+    );
+
+    const member = await getMember({
+      databases,
+      userId: user.$id,
+      workspaceId: project.workspaceId,
+    });
+
+    if (!member) {
+      return null;
+    }
+
+    return project;
+  } catch {
+    return null;
+  }
+};
