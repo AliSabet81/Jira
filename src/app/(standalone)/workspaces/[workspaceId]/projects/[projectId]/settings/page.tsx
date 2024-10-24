df964be27bc4cca54bfec9eaba2ec357552b40b2
AliSabet81 <sabet.farshid1381@gmail.com>
2024-10-24 09:10:24 +0330

    project settings page

---
+++ src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
@@ -0,0 +1,28 @@
+import { redirect } from "next/navigation";
+
+import { getCurrent } from "@/features/auth/queries";
+import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
+import { getWorkspace } from "@/features/workspaces/queries";
+import { getProject } from "@/features/projects/queries";
+
+interface ProjectIdSettingsPageProps {
+  params: {
+    projectId: string;
+  };
+}
+
+const ProjectIdSettingsPage = async ({
+  params,
+}: ProjectIdSettingsPageProps) => {
+  const user = await getCurrent();
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  const initialValues = await getProject({ projectId: params.projectId });
+
+  return <div className="w-full lg:max-w-xl"></div>;
+};
+
+export default ProjectIdSettingsPage;
