dbe77d2bbcc98534a9c62e81c133dec926bd89c6
AliSabet81 <sabet.farshid1381@gmail.com>
2024-10-24 13:34:42 +0330

    fixingtasks view

--- src/features/tasks/api/use-get-tasks.ts
+++ src/features/tasks/api/use-get-tasks.ts
@@ -1,17 +1,45 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { client } from "@/lib/rpc";
+import { TaskStatus } from "../types";
 
 interface useGetTasksProps {
   workspaceId: string;
+  projectId?: string | null;
+  assigneeId?: string | null;
+  status?: TaskStatus | null;
+  dueData?: string | null;
+  search?: string | null;
 }
 
-export const useGetTasks = ({ workspaceId }: useGetTasksProps) => {
+export const useGetTasks = ({
+  workspaceId,
+  projectId,
+  status,
+  search,
+  assigneeId,
+  dueData,
+}: useGetTasksProps) => {
   const query = useQuery({
-    queryKey: ["tasks", workspaceId],
+    queryKey: [
+      "tasks",
+      workspaceId,
+      projectId,
+      status,
+      search,
+      assigneeId,
+      dueData,
+    ],
     queryFn: async () => {
       const response = await client.api.tasks.$get({
-        query: { workspaceId },
+        query: {
+          workspaceId,
+          projectId: projectId ?? undefined,
+          status: status ?? undefined,
+          search: search ?? undefined,
+          assigneeId: assigneeId ?? undefined,
+          dueData: dueData ?? undefined,
+        },
       });
 
       if (!response.ok) {
--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -3,14 +3,29 @@
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { PlusIcon } from "lucide-react";
+import { Loader, PlusIcon } from "lucide-react";
 import { useCreateTaskModal } from "../hooks/use-create-task-modal";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useGetTasks } from "../api/use-get-tasks";
+import { useQueryState } from "nuqs";
 
 const TaskViewSwitcher = () => {
+  const [view, setView] = useQueryState("task-view", { defaultValue: "table" });
+
+  const workspaceId = useWorkspaceId();
+
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
+    workspaceId,
+  });
+
   const { open } = useCreateTaskModal();
 
   return (
-    <Tabs className="flex-1 w-full border rounded-lg">
+    <Tabs
+      defaultValue={view}
+      onValueChange={setView}
+      className="flex-1 w-full border rounded-lg"
+    >
       <div className="h-full flex flex-col overflow-auto p-4">
         <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
           <TabsList className="w-full lg:w-auto">
@@ -31,11 +46,17 @@ const TaskViewSwitcher = () => {
         <DottedSeparator className="my-4" />
         Data Filters
         <DottedSeparator className="my-4" />
-        <>
-          <TabsContent value="table">Data Table</TabsContent>
-          <TabsContent value="canban">Data Canban</TabsContent>
-          <TabsContent value="calendat">Data Calendar</TabsContent>
-        </>
+        {isLoadingTasks ? (
+          <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
+            <Loader className="size-5 animate-spin text-muted-foreground" />
+          </div>
+        ) : (
+          <>
+            <TabsContent value="table">Data Table</TabsContent>
+            <TabsContent value="canban">Data Canban</TabsContent>
+            <TabsContent value="calendat">Data Calendar</TabsContent>
+          </>
+        )}
       </div>
     </Tabs>
   );
