67da499fce9fed2c7969078286770d53c7734d22
AliSabet81 <sabet.farshid1381@gmail.com>
2024-10-24 12:46:30 +0330

    create task form wrapper

--- src/features/tasks/components/create-task-modal.tsx
+++ src/features/tasks/components/create-task-modal.tsx
@@ -3,13 +3,14 @@
 import { ResponsiveModal } from "@/components/responsive-modal";
 
 import { useCreateTaskModal } from "../hooks/use-create-task-modal";
+import CreateTaskFormWrapper from "./create-task-form-wrapper";
 
 export const CreateTaskModal = () => {
   const { isOpen, setIsOpen, close } = useCreateTaskModal();
 
   return (
     <ResponsiveModal open={isOpen} onOpenChange={setIsOpen}>
-      <div>{/* <CreateTaskForm onCancel={close} /> */}</div>
+      <CreateTaskFormWrapper onCancel={close} />
     </ResponsiveModal>
   );
 };
